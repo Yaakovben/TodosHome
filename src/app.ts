75d9b0c5339d6e569c811494bcae6f30d8d2f42f
Yaakovben <yaakovbenyizchak1@gmail.com>
2024-10-09 11:10:28 +0300

    create connection to MongoDB

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,24 @@ import exp,{Express}  from "express";
 import "dotenv/config"
 import usersRouter from './routers/usersRouter'
 import todosRouter from './routers/todosRouter'
+import connectToMongo from "./config/DBConnect";
+
 
 const app:Express = exp()
 
 app.use(exp.json())
 
-app.use('/user',usersRouter )
-app.use('/todo',todosRouter )
+connectToMongo()
+
+app.use('/user',usersRouter)
+app.use('/todo',todosRouter)
 
 
 const PORT:number = (process.env.PORT || 1010) as number
 app.listen(PORT,() => { console.log(`Server is up and running, feel free to visit at http://localhost:${PORT}`);
 })
 
+  
    
 
 
